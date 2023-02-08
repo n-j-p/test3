dec64646a6253e949f2398e293d1f796f7bf4d45
n-j-p <nicholas.j.potter@gmail.com>
2023-02-08 18:20:36 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='test3',
+	version='0.1',
+	description='test module',
+	author='n-j-p',
+	author_email='example@github.com',
+	packages=['test3'],
+	install_requires=['pandas']
+)
